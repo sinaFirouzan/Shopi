ecc52fa3f5c7290be82bd99c3c85b952fcc33200
sinaFiroozan88 <sinafiroozan@gmail.com>
2025-02-06 21:52:28 +0330

    completing get_brands & get_categories APIs

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,6 @@ from products import views
 
 urlpatterns = [
     path('', views.get_products, name='get_products'),
-    path('get_brands', views.get_brands, name='get_brands'),
-    path('get_categories', views.get_categories, name='get_categories'),
+    path('get_brands/', views.get_brands, name='get_brands'),
+    path('get_categories/', views.get_categories, name='get_categories'),
 ]
